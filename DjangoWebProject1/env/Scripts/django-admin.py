e0bd0d84d7ef67924b7212ae7e05f7e1a1958f1f
Mitsufiro <83647115+Mitsufiro@users.noreply.github.com>
2022-12-18 19:17:41 +0300

    Delete DjangoWebProject1/env directory

--- DjangoWebProject1/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Ilyal\source\repos\DjangoWebProject1\DjangoWebProject1\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
